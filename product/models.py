14b0c0536f1cab93e62d235b1c15b797cc980b36
Hjortur Helgi <hjorturt21@ru.is>
2023-04-30 20:55:38 +0000

    Created models in the "product" application.

--- product/models.py
+++ product/models.py
@@ -3,24 +3,33 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=50)
 
 
 class Topping(models.Model):
-    pass
+    name = models.CharField(max_length=50)
 
 
 class Pizza(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    toppings = models.ManyToManyField(Topping)
+    categories = models.ManyToManyField(Category)
+    price = models.IntegerField()
 
 
 class Offers(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    pizzas = models.ManyToManyField(Pizza)
+    price = models.IntegerField()
 
 
 class PizzaImg(models.Model):
-    pass
+    image = models.CharField(max_length=9999)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
 
 
 class OfferImg(models.Model):
-    pass
+    image = models.CharField(max_length=9999)
+    offer = models.ForeignKey(Offers, on_delete=models.CASCADE)
