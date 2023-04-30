e07493555ba1baf2a4d0e008b50a73aead32740a
kjarri007 <102475373+kjarri007@users.noreply.github.com>
2023-04-30 20:09:30 +0000

    created View stubs for pizza and offer

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def pizza_index(request):
+    return None
+
+
+def offer_index(request):
+    return None
+
+
+def pizza_detail(request, pizza_id):
+    return None
+
+
+def offer_detail(request, offer_id):
+    return None
