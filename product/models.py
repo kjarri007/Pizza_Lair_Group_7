0f4e456e24ecbb7f7deae7a05d07431903a7f19a
Hjortur Helgi <hjorturt21@ru.is>
2023-04-30 20:12:28 +0000

    Created stubbs in product.models.py application.

--- product/models.py
+++ product/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    pass
+
+
+class Topping(models.Model):
+    pass
+
+
+class Pizza(models.Model):
+    pass
+
+
+class Offers(models.Model):
+    pass
+
+
+class PizzaImg(models.Model):
+    pass
+
+
+class OfferImg(models.Model):
+    pass
