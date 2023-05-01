ffbabe5b53f2456456c311db05748ab5307350fe
kjarri007 <102475373+kjarri007@users.noreply.github.com>
2023-05-01 13:35:34 +0000

    Removed "name" attribute from "Profile" model

--- user/models.py
+++ user/models.py
@@ -5,5 +5,5 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
     image = models.CharField(max_length=9999)
+# Á eftir að Migrate-a!!!!!!!!
