f2c2356201560eda14fa5eea9bbfa417832d059d
kjarri007 <102475373+kjarri007@users.noreply.github.com>
2023-05-02 12:01:21 +0000

    Implemented a search feature for pizzas

--- static/js/pizzas.js
+++ static/js/pizzas.js
@@ -1,25 +1,24 @@
 $(document).ready(function(){
     $('#search-btn').on('click', function(e){
         e.preventDefault();
-        let searchText = $('search-box').val();
+        let searchText = $('#search-bar').val();
         $.ajax({
-            url: '/pizzas?search_filter=' + searchText,
+            url: '/order/pizzas?search_filter=' + searchText,
             type: 'GET',
             success: function(resp) {
                 let newHtml = resp.data.map(d => {
-                    return `<div class="well candy">
-                                <a href="/pizzas/${d.id}">
-                                    <img class="pizza-img" src="${d.firstImage}" />
+                    return `<div class="well pizza">
+                                <a href="/order/pizzas/${d.id}">
+                                    <img class="card-img-top" src="${d.firstImage}" />
                                     <h4>${d.name}</h4>
                                     <p>${d.description}</p>
                                 </a>
-                        </div>`
+                            </div>`
                 });
-                $('.pizzas').html(newHtml.join(''));
-                $('search-box').val('');
+                $('.pizza-catalog').html(newHtml.join(''));
+                $('#search-bar').val('');
             },
-            error: function(xhr, status, error){
-                // TODO: Show toaster
+            error: function(xhr, status, error) {
                 console.error(error);
             }
         })
