ddc49011ab76c54bcf33a0dec4ad978f98300d93
Hjortur Helgi <hjorturt21@ru.is>
2023-05-04 11:33:43 +0000

    Created paths for checkout process. Changed the name of "payment info.html" to "payment_info.html"

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def cart(request):
+    return render(request, 'checkout/cart.html')
+
+
+def contact(request):
+    return render(request, 'checkout/contact_info.html')
+
+
+def payment(request):
+    return render(request, 'checkout/payment_info.html')
+
+
+def review(request):
+    return render(request, 'checkout/review_order.html')
+
+
+def process(request):
+    return render(request, 'checkout/thank_you.html')
