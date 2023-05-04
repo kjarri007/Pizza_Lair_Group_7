bda529ce828b6b91beef31cbedd50e2161cc6e5d
kjarri007 <102475373+kjarri007@users.noreply.github.com>
2023-05-04 22:01:58 +0000

    unfinished ajax implementation!

--- product/views.py
+++ product/views.py
@@ -8,43 +8,25 @@ from product.models import Pizza
 # Create your views here.
 
 def pizza_index(request):
+    all_pizzas = Pizza.objects.all().order_by('price')
+    if 'sort_by' not in request.GET:
+        all_categories = models.Category.objects.all()
+        return render(request, 'product/pizza_index.html',
+                      context={"all_pizzas": all_pizzas, "all_categories": all_categories})
+
+    order = request.GET['order']
+
     # If the user searches pizzas by name
     if 'search_filter' in request.GET:
         search_filter = request.GET['search_filter']
-        pizzas = [{
-            'id': pizza.id,
-            'name': pizza.name,
-            'description': pizza.description,
-            'firstImage': pizza.pizzaimg_set.first().image
-        } for pizza in Pizza.objects.filter(name__icontains=search_filter)]
-        return JsonResponse({'data': pizzas})
+        filtered_pizzas = all_pizzas.filter(name__icontains=search_filter).order_by(order)
+        return JsonResponse({"data": filtered_pizzas})
 
     # If the user presses a category button to filter pizzas by category
     elif 'category_filter' in request.GET:
-        category_id = request.GET['category_filter']
-        if category_id == '0':
-            pizzas = [{
-                'id': pizza.id,
-                'name': pizza.name,
-                'description': pizza.description,
-                'firstImage': pizza.pizzaimg_set.first().image
-            } for pizza in Pizza.objects.all().order_by('price')]
-        else:
-            category = get_object_or_404(models.Category, pk=category_id)
-            pizzas = [{
-                'id': pizza.id,
-                'name': pizza.name,
-                'description': pizza.description,
-                'firstImage': pizza.pizzaimg_set.first().image
-            } for pizza in Pizza.objects.filter(categories=category)]
-
-        return JsonResponse({'data': pizzas})
-
-    # If the user is visiting the link and not filtering in any way
-    all_pizzas = models.Pizza.objects.all().order_by("price")
-    all_categories = models.Category.objects.all()
-    return render(request, 'product/pizza_index.html',
-                  context={"all_pizzas": all_pizzas, "all_categories": all_categories})
+        category = request.GET['category_filter']
+        filtered_pizzas = all_pizzas.filter(categories__exact=category).order_by(order)
+        return JsonResponse({"data": filtered_pizzas})
 
 
 def offer_index(request):
--- static/js/pizzas.js
+++ static/js/pizzas.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
     $('#search-btn').on('click', function(e){
         e.preventDefault();
         let searchText = $('#search-bar').val();
+        // Check the state of the order switch!
+        let order
+        let my_checkbox = $('#flexSwitchCheckDefault')
+        if (my_checkbox.prop('checked')) {
+            order = 'name'
+        } else {
+            order = 'price'
+        }
         $.ajax({
             url: '/order/pizzas?search_filter=' + searchText,
             type: 'GET',
@@ -28,12 +36,18 @@ $(document).ready(function(){
     $('.category-button').on('click', function(e) {
         e.preventDefault();
         let categoryId = $(this).attr('id');
+        // Check the state of the order switch!
+        let order
+        let my_checkbox = $('#flexSwitchCheckDefault')
+        if (my_checkbox.prop('checked')) {
+            order = 'name'
+        } else {
+            order = 'price'
+        }
         $.ajax({
-            url: '/order/pizzas?category_filter=' + categoryId,
+            url: '/order/pizzas?category_filter=' + categoryId + '&' + 'order_by' + order,
             type: 'GET',
             success: function(resp){
-                console.log(categoryId)
-                console.log(typeof(categoryId))
                 let newHtml = resp.data.map(d => {
                     return `<div class=" well card pizza" style="width: 24rem;">
                                 <a href="/order/pizzas/${d.id}" class="link-offset-2 link-underline link-underline-opacity-0">
@@ -51,6 +65,33 @@ $(document).ready(function(){
                 console.error(error);
             }
         })
-    })
+    });
+    $('#my-checkbox').change(function() {
+        if (this.checked) {
+            // send AJAX request for when checkbox is checked
+            $.ajax({
+                url: '/my-endpoint-on',
+                type: 'POST',
+                success: function(data) {
+                    console.log('Checkbox is checked');
+                },
+                error: function(xhr, status, error) {
+                    console.error(error);
+                }
+            });
+        } else {
+            // send AJAX request for when checkbox is not checked
+            $.ajax({
+                url: '/my-endpoint-off',
+                type: 'POST',
+                success: function(data) {
+                    console.log('Checkbox is not checked');
+                },
+                error: function(xhr, status, error) {
+                    console.error(error);
+                }
+            });
+        }
+    });
 });
 
