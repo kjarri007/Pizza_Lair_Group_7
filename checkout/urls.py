0f50193da9994e8603c93b34d275a8800786bb54
Kristinnfb <kristinnb21@ru.is>
2023-05-04 10:02:04 +0000

    Created checkout app and all related templates

---
+++ checkout/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("cart", views.cart, name="cart"),
+    path("contact", ContactView.as_view(template_name="checkout/contact_info.html"), name="contact_info"),
+    path("payment", PaymentView.as_view(next_page="review_order"), name="payment_info"),
+    path("review", ReviewView.as_view(next_page="thank_you"), name="review_order"),
+    path("contact", ThanksView.as_view(template_name="checkout/thank_you.html"), name="thank_you"),
+
+]
