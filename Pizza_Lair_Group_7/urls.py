fdc48b2d7a9bb1bb831b01ad13fbe3abdea6ee44
Kristinnfb <kristinnb21@ru.is>
2023-05-04 10:55:32 +0000

    Added a cart to the header and made some changes on the cart template

--- Pizza_Lair_Group_7/urls.py
+++ Pizza_Lair_Group_7/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("order/", include("product.urls")),
     path("user/", include("user.urls")),
-    path("checkout/", include("checkout.urls"))
+    # path("checkout/", include("checkout.urls"))
 ]
