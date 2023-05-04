ceba672f80dc579ee1bc203aeaaa111cb9cda28b
Hjortur Helgi <hjorturt21@ru.is>
2023-05-04 21:49:41 +0000

    Created static/js/add_to_cart.js file and that is on its way...
    Changed the name on the button in cart.html.
    
    Created classes Product and Cart in user/models.py and more..
    
    Changed input tag to button tag in templates/product/pizza_detail.html.
    
    Deleted unused paths in checkout/views.py.

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,12 +3,9 @@ from . import views
 
 urlpatterns = [
     path("cart/", views.cart, name="cart"),
-    # path("contact/", ContactView.as_view(template_name="checkout/contact_info.html"), name="contact_info"),
+    path("add_to_cart/", views.add_to_cart, name="add_to_cart"),
     path("contact/", views.contact, name="contact_info"),
-    # path("payment/", PaymentView.as_view(next_page="review_order"), name="payment_info"),
     path("payment/", views.payment, name="payment_info"),
-    # path("review/", ReviewView.as_view(next_page="thank_you"), name="review_order"),
     path("review/", views.review, name="review_oder"),
-    # path("process/", ThanksView.as_view(template_name="checkout/thank_you.html"), name="thank_you"),
     path("process/", views.process, name="thank_you")
 ]
