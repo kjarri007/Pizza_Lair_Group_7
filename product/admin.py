6d6daadb63b936b6a0d8d60204ccc91b1f298f29
Hjortur Helgi <hjorturt21@ru.is>
2023-05-05 14:12:11 +0000

    small changed regarding database schema.

--- product/admin.py
+++ product/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+admin.site.register(Category)
 admin.site.register(Pizza)
 admin.site.register(Offer)
 admin.site.register(Topping)
-admin.site.register(Category)
-admin.site.register(PizzaImg)
-admin.site.register(OfferImg)
+admin.site.register(ProductImg)
