297a684f637d00c49647f8b291d7610549799d0d
Hjortur Helgi <hjorturt21@ru.is>
2023-05-08 22:45:08 +0000

    Small fixes in html. implementing total price of the same product.

--- product/urls.py
+++ product/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path("", views.front_page, name="main_page"),
     path("pizzas/", views.pizza_index, name="pizza_index"),
     path("offer/", views.offer_index, name="offer_index"),
-    # path("pizzas/<int:pizza_id>, <int:pizza_quantity>", views.pizza_detail, name="pizza_detail"),
     path("pizzas/<int:pizza_id>", views.pizza_detail, name="pizza_detail"),
     path("offer/<int:offer_id>", views.offer_detail, name="offer_detail"),
 ]
