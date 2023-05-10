04d680c9a60e407408f88f6659248d460b9d4c01
kjarri007 <102475373+kjarri007@users.noreply.github.com>
2023-05-10 14:29:49 +0000

    Changed the type of "continue shopping" to button

--- checkout/forms/payment_detail.py
+++ checkout/forms/payment_detail.py
@@ -1,5 +1,7 @@
-from django.forms import ModelForm, widgets
+from django.forms import ModelForm, widgets, ValidationError
 from checkout.models import PaymentDetails
+from django.utils import timezone
+from datetime import datetime, timezone
 
 
 class PaymentDetailsForm(ModelForm):
@@ -18,3 +20,27 @@ class PaymentDetailsForm(ModelForm):
             "expiration_date": widgets.DateInput(attrs={'class': 'form-control'}),
             "cvc": widgets.TextInput(attrs={'class': 'form-control'}),
         }
+
+        labels = {
+            "card_holder": "Full Name",
+            "card_number": "Card Number",
+            "expiration_date": "Exp.Date",
+            "cvc": "CVC"
+        }
+
+    @staticmethod
+    def clean_card_number(self):
+        card_number = PaymentDetails.card_number
+        if not card_number.isdigit():
+            raise ValidationError("Please enter only digits.")
+        if not len(card_number) == 16:
+            raise ValidationError("A credit card number must be exactly 16 digits long.")
+        return card_number
+
+    @staticmethod
+    def clean_expiration_date(self):
+        expiration_date = datetime.strptime(str(PaymentDetails.expiration_date), "%m-%y")
+        today = timezone.now()
+        if not expiration_date < today:
+            raise ValidationError("This is not a valid expiration date.")
+        return expiration_date
