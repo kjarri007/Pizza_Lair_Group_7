c36730f12799a3e6aaef48a4f51f1965822b6102
kjarri007 <102475373+kjarri007@users.noreply.github.com>
2023-05-12 16:24:49 +0000

    fixed some issues

--- checkout/views.py
+++ checkout/views.py
@@ -30,15 +30,11 @@ def contact_info(request):
 def payment_info(request):
     user_payment_info = PaymentDetails.objects.filter(user=request.user).first()
     if request.method == "POST":
-        if request.POST["payment-method"] == 'credit-card':
-            form = PaymentDetailsForm(instance=user_payment_info, data=request.POST)
-            if form.is_valid():
-                user_payment_info = form.save(commit=False)
-                user_payment_info.user = request.user
-                user_payment_info.save()
-        elif request.POST["payment-method"] == 'pickup':
-            if user_payment_info:
-                user_payment_info.delete()
+        form = PaymentDetailsForm(instance=user_payment_info, data=request.POST)
+        if form.is_valid():
+            user_payment_info = form.save(commit=False)
+            user_payment_info.user = request.user
+            user_payment_info.save()
         if "contact_info" in request.POST:
             return redirect("contact_info")
         elif "review_order" in request.POST:
@@ -51,14 +47,12 @@ def payment_info(request):
 
 @login_required
 def review_step(request):
-    if request.method == "POST":
-        user_cart = Cart.objects.filter(user=request.user).first()
-        user_payment_info = PaymentDetails.objects.filter(user=request.user).first()  # needs to be erased when POST
-        user_contact_info = ContactInfo.objects.filter(user=request.user).first()  # needs to be erased when POST
-        return render(request, "checkout/review_order.html",
-                      context={"cart": user_cart, "contact_info": user_contact_info, "payment_info": user_payment_info})
-    elif request.method == "GET":
-        return redirect("main_page")
+    user_cart = Cart.objects.filter(user=request.user).first()
+    user_payment_info = PaymentDetails.objects.filter(user=request.user).first()  # needs to be erased when POST
+    user_contact_info = ContactInfo.objects.filter(user=request.user).first()  # needs to be erased when POST
+    return render(request, "checkout/review_order.html",
+                  context={"cart": user_cart, "contact_info": user_contact_info, "payment_info": user_payment_info})
+
 
 @login_required
 def confirmation(request):
