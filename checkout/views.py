6653fd102d63e025a62ff89829e1bc2e4378f962
kjarri007 <102475373+kjarri007@users.noreply.github.com>
2023-05-12 15:42:46 +0000

    eitthvað

--- checkout/views.py
+++ checkout/views.py
@@ -30,15 +30,19 @@ def contact_info(request):
 def payment_info(request):
     user_payment_info = PaymentDetails.objects.filter(user=request.user).first()
     if request.method == "POST":
-        form = PaymentDetailsForm(instance=user_payment_info, data=request.POST)
-        if form.is_valid():
-            user_payment_info = form.save(commit=False)
-            user_payment_info.user = request.user
-            user_payment_info.save()
-            if "contact_info" in request.POST:
-                return redirect("contact_info")
-            elif "review_order" in request.POST:
-                return redirect("review_order")
+        if request.POST["payment-method"] == 'credit-card':
+            form = PaymentDetailsForm(instance=user_payment_info, data=request.POST)
+            if form.is_valid():
+                user_payment_info = form.save(commit=False)
+                user_payment_info.user = request.user
+                user_payment_info.save()
+        elif request.POST["payment-method"] == 'pickup':
+            if user_payment_info:
+                user_payment_info.delete()
+        if "contact_info" in request.POST:
+            return redirect("contact_info")
+        elif "review_order" in request.POST:
+            return redirect("review_order")
     else:
         form = PaymentDetailsForm(instance=user_payment_info)
     context = {"form": form}
@@ -47,14 +51,14 @@ def payment_info(request):
 
 @login_required
 def review_step(request):
-    user_cart = Cart.objects.filter(user=request.user).first()
-    user_payment_info = PaymentDetails.objects.filter(user=request.user).first()  # needs to be erased when POST
-    user_contact_info = ContactInfo.objects.filter(user=request.user).first()  # needs to be erased when POST
     if request.method == "POST":
-        pass
-    return render(request, "checkout/review_order.html",
-                  context={"cart": user_cart, "contact_info": user_contact_info, "payment_info": user_payment_info})
-
+        user_cart = Cart.objects.filter(user=request.user).first()
+        user_payment_info = PaymentDetails.objects.filter(user=request.user).first()  # needs to be erased when POST
+        user_contact_info = ContactInfo.objects.filter(user=request.user).first()  # needs to be erased when POST
+        return render(request, "checkout/review_order.html",
+                      context={"cart": user_cart, "contact_info": user_contact_info, "payment_info": user_payment_info})
+    elif request.method == "GET":
+        return redirect("main_page")
 
 @login_required
 def confirmation(request):
